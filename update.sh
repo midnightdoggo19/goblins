3dc976b5b18364252d2fa02682cefcf2a29404bd
Midnight Doggo <git@midnightdoggo19.com>
2026-06-11 20:37:57 +0100

    Add mod list to README.md and a script to do that automatically

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Takes modrinth.index.json as input (the manifest of a given .mrpack)
+
+echo "# Goblins" > README.md
+echo "The Goblins modpack." >> README.md
+echo "" >> README.md
+echo "# Contents" >> README.md
+
+jq --indent 0 '.. | select(type == "object" and has("downloads")).downloads' modrinth.index.json | sed s/\]/\/ | sed s/\\[/*\ / | sed s/\"// >> README.md
+cat README.md
