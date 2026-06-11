aa8bd00a4ffb60196cc33aaafe294cfdf5828f68
Midnight Doggo <git@midnightdoggo19.com>
2026-06-11 20:41:19 +0100

    Remove trailing quotation from mod list

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ echo "The Goblins modpack." >> README.md
 echo "" >> README.md
 echo "# Contents" >> README.md
 
-jq --indent 0 '.. | select(type == "object" and has("downloads")).downloads' modrinth.index.json | sed s/\]/\/ | sed s/\\[/*\ / | sed s/\"// >> README.md
+jq --indent 0 '.. | select(type == "object" and has("downloads")).downloads' modrinth.index.json | sed s/\]/\/ | sed s/\\[/*\ / | sed s/\"// | sed s/\"// >> README.md
 cat README.md
